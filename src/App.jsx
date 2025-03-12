666d9db4fe4cf226e5df89b2debe3025da6757e0
Jeev Pangli <jspanglez@gmail.com>
2025-03-12 18:04:32 +0000

    Add components to return statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React, { useState } from 'react'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import SearchBar from './SearchBar/SearchBar'
+import Button from './Button/Button'
+import SearchResults from './SearchResults/SearchResults'
+import Playlist from './Playlist/Playlist';
 
 function App() {
-
   return (
-    <h1>Jammin'!</h1>
+    <div className='appContainer'>
+      <header>Jammin'</header>
+      <SearchBar placeHolder="Enter a song..."/>
+      <Button text={<> Search <FontAwesomeIcon icon={faMagnifyingGlass} className='icon'/></>} />
+      <div className='bottomContainer'>
+        <SearchResults />
+        <Playlist />
+      </div>
+    </div>
   )
 }
 
