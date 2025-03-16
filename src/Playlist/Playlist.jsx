cb61bb080bbb8f29132e34ef1ab2db450de92be4
Jeev Pangli <jspanglez@gmail.com>
2025-03-16 17:06:14 +0000

    Dynamically render each track selected for the playlist

--- src/Playlist/Playlist.jsx
+++ src/Playlist/Playlist.jsx
@@ -5,7 +5,14 @@ import styles from './Playlist.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpotify } from '@fortawesome/free-brands-svg-icons';
 
-function Playlist() {
+function Playlist(props) {
+  const [name, setName] = useState('')
+
+  const updateSongName = (event) => {
+    setName(event.target.value)
+    props.updatePlaylistName(event.target.value)
+  }
+  
   return (
     <div className={styles.playlistContainer}>
       <SearchBar 
@@ -13,6 +20,24 @@ function Playlist() {
       value={name}
       inputHandler={updateSongName}
       className={styles.playlistName}/>
+      <div>
+        {props.playlist.map((track, index) => (
+          <div key={index} className={styles.track}>
+            <h2>{track.name}</h2>
+            <Button 
+            text="-"
+            handleFunction={() => props.removeTrackFromPlaylist(index)}
+            className={styles.removeButton}/>
+            <h3>{track.artist}</h3>
+            <h3>{track.album}</h3>
+            <hr />
+          </div>
+        ))}
+      </div>
+      <Button 
+      text={<>Save to Spotify <FontAwesomeIcon icon={faSpotify} /></>} 
+      className={styles.saveButton}
+      handleFunction={props.savePlaylist}/>
     </div>
   )
 }
