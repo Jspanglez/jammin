e35f2d451b0f850cebe8aaa5c59d6c6724fb078b
Jeev Pangli <jspanglez@gmail.com>
2025-03-16 16:58:48 +0000

    Create an event listener for the button

--- src/Track/Track.jsx
+++ src/Track/Track.jsx
@@ -3,16 +3,22 @@ import styles from './Track.module.css'
 import Button from "../Button/Button";
 
 function Track(props) {
- return (
-  <div className={styles.trackDiv}>
-    <h2>{props.name}</h2>
-    <Button 
-    text="+"
-    className={styles.addButton}/>
-    <h3>{props.artist}</h3>
-    <hr />
-  </div>
- )
+  const handleButton = () => {
+    props.addTrackToPlaylist({ id: props.id, name: props.name, artist: props.artist, album: props.album, uri: props.uri })
+  }
+
+  return (
+    <div className={styles.trackDiv}>
+      <h2>{props.name}</h2>
+      <Button 
+      text="+"
+      handleFunction={handleButton}
+      className={styles.addButton}/>
+      <h3>{props.artist}</h3>
+      <h3>{props.album}</h3>
+      <hr />
+    </div>
+  )
 }
 
 export default Track
